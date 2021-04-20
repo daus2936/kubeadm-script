211a91590d19a7953bf2dbdb904031727499c599
daus2936 <dausadinegoro@gmail.com>
2021-04-20 19:19:19 +0700

    script revision

--- ccluster.sh
+++ ccluster.sh
@@ -11,7 +11,7 @@ kubeadm init --pod-network-cidr=$NETWORK_CIDR  --apiserver-advertise-address=$MA
 su - $USERNAME <<EOF
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chown $USERNAME:$USERNAME $HOME/.kube/config
 EOF
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
