5dbb4a3d5a3a79b48f2c70d6dd1aee8974b09577
daus2936 <dausadinegoro@gmail.com>
2021-04-20 16:57:42 +0700

    first commit

---
+++ ccluster.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Run as root
+
+source variable.sh
+
+#create cluster
+
+kubeadm init --pod-network-cidr=$NETWORK_CIDR  --apiserver-advertise-address=$MASTER_IP
+
+su - $USERNAME
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
